8572e7184af33e40d2a3687ec46ad11f12e59881
Rajnish Patel <rajnish17100042@gmail.com>
2022-03-21 10:31:01 +0530

    installed angular and setup base app

--- router/routes.js
+++ router/routes.js
@@ -23,6 +23,7 @@ router.get("/", (req, res) => {
   res.status(200).json({ success: true, message: "Hello from the Home Page" });
 });
 
+//common registration route for student,librarian and admin
 router.post("/register", async (req, res) => {
   // calling function for input validation
   const isInputValidated = validateInput(req);
@@ -32,7 +33,11 @@ router.post("/register", async (req, res) => {
     res.json({ success: false, message: "Please fill the data properly" });
   } else {
     try {
-      const userExist = await Registration.findOne({ email: req.body.email });
+      //do not allow duplicate user having same role and email id
+      const userExist = await Registration.findOne({
+        email: req.body.email,
+        role: req.body.role,
+      });
       console.log(userExist);
       if (userExist) {
         return res.json({
@@ -59,6 +64,72 @@ router.post("/register", async (req, res) => {
         message: err,
       });
     }
+  }
+});
+
+// role based login system
+router.post("/login", async (req, res) => {
+  // first get the details send by the client
+  const { email, password, role } = req.body;
+
+  // server side input validation
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      success: false,
+      message: "Please fill all the details correctly",
+    });
+  }
+
+  // check if user exist based on the email and role
+  try {
+    const userExist = await Registration.findOne({ email, role });
+    console.log(userExist);
+    if (!userExist) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User NOT exists" });
+    } else {
+      //compare the password
+      bcrypt.compare(password, userExist.password, (err, result) => {
+        if (err) {
+          // throw err;
+          return res
+            .status(400)
+            .json({ success: false, message: "OPPS !! Something went wrong" });
+        } else if (!result) {
+          return res
+            .status(400)
+            .json({ success: false, message: "OPPS !! Something went wrong" });
+        }
+
+        //generate access token or JWT for this user
+        //syntax   jwt.sign(payload,secretKey,expiry,callbackFunction);
+        // console.log(userExist);
+        jwt.sign({ userExist }, secretKey, (err, token) => {
+          if (err) {
+            // throw err;
+            return res.status(400).json({
+              success: false,
+              message: "OPPS !! Something went wrong",
+            });
+          }
+
+          res.cookie("jwtoken", token, {
+            expires: new Date(Date.now() + 100000000000), //time in millisecond  100second
+            httpOnly: true,
+          });
+          // console.log(token);
+          res.json({
+            success: true,
+            access_token: token,
+          });
+        });
+      });
+    }
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ success: false, message: "OPPS!! something went wrong" });
   }
 });
 
