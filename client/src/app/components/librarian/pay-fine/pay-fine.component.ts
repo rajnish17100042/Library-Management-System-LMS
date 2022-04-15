38bc6d678d8efdfcc54bc41b3a197bb4a1aec944
Rajnish Patel <rajnish17100042@gmail.com>
2022-04-15 16:57:25 +0530

    updated

--- client/src/app/components/librarian/pay-fine/pay-fine.component.ts
+++ client/src/app/components/librarian/pay-fine/pay-fine.component.ts
@@ -79,7 +79,7 @@ export class PayFineComponent implements OnInit {
          
        }
        else{
-            this.flashMessage.show("Something went wrong",{cssClass:'alert-danger',timeout:3000});
+            this.flashMessage.show(data.message,{cssClass:'alert-danger',timeout:3000});
             
        }
       }
